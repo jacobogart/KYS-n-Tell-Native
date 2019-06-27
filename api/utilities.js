1b8bbbdd6dcc9068c4e297ce4148f7581d5bf1e0
Jacob Bogart <45608610+jacobogart@users.noreply.github.com>
2019-06-27 13:39:34 -0600

    Delete utilities.js

--- api/utilities.js
+++
@@ -1,9 +0,0 @@
-export const url = 'https://locator.hiv.gov/data?';
-
-export const geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-
-export const googleRoot = 'https://maps.googleapis.com/maps/api/';
-
-export const googleKey = 'AIzaSyDHKnndIp7I8oymrxO0sDwpQUdZvfN1sqc';
-
-export const IP = '10.1.3.83';
